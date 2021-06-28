156e0cd5f6e91b6b2dc4dcb86f72820835d4ab00
@Jishnu.Nair1 <Jishnu.Nair1@ust.com>
2021-06-28 16:36:13 +0530

    commit

--- Testing/src/main/java/Main.java
+++ Testing/src/main/java/Main.java
@@ -1,34 +1,19 @@
 package main.java;
 
 import java.util.Scanner;
+public class Main {  
 
-public class Main {
+	public static int findMax(int arr[]){  
+        int max=0;  
+        for(int i=1;i<arr.length;i++){  
+            if(max<arr[i])  
+                max=arr[i];  
+        }  
+        return max;  
+    }  
 
-	public static void main(String[] args) {
-		
-
-		String repeat = "y";
-		Scanner sc = new Scanner(System.in);
-		do {
-		
-		System.out.println("String Joiner");
-		System.out.println("Enter First String");
-		String str1 = new String(sc.nextLine());
-		System.out.println("Enter the Second Line");
-		String str2 =  new String(sc.nextLine());
-		System.out.println("Combined String");
-		System.out.println(combiner(str1,str2));
-		System.out.println("Do you want to repeat:(y,n)");
-		repeat = sc.nextLine();
-		
-
-	}while(repeat.equalsIgnoreCase("y"));
-		sc.close();
-		System.out.println("End of Program");
-	}
-
-	public static String combiner(String str1, String str2) {
-		return str1+str2;
-	}
-
-}
+    public static int cube(int n){  
+        return n*n*n;  
+    }  
+   
+}  
--- Testing/src/test/java/MainTests.java
+++ Testing/src/test/java/MainTests.java
@@ -1,83 +1,45 @@
 package test.java;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
-
-import org.junit.Assert;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import main.java.Main;
-
-@DisplayName("Main should")
-class MainTests {
-	static String[] str = new String[6];
-	
-	@BeforeAll
-	static void setup()
-	{
-		System.out.println("Before All");
-		str[0] = "asdf";
-		str[1] = "ghjk";
-		str[2] = "qwer";
-		str[3] = "tyui";
-		str[4] = "";
-		str[5] = "";
-	}
-	@BeforeEach
-	void init()
-	{
-		System.out.println("Before Each");		
-	}
-	
-	@Test
-	@DisplayName("Throw an exception")
-	void exceptionTest()
-	{
-		assertThrows(NumberFormatException.class,() -> Integer.parseInt(str[4]));	
-
-	}
-
-	@Test
-	@DisplayName("Check string joined properly1")
-	void test1() {
-		
-		String expected = new String(str[0] + str[1]);
-		String actual = Main.combiner(str[0], str[1]);
-		assertNotNull(actual);
-		assertEquals("Check your code..... ",expected, actual);
-	}
-	@Test
-	@Disabled
-	@DisplayName("Check string joined properly2")
-	void test2() {
-		
-		String expected = new String(str[1] + str[2]);
-		String actual = Main.combiner(str[1], str[2]);
-		assertNotNull(actual);
-		assertEquals("Check your code..... ",expected, actual);
-		
-	}
-	
-	@AfterEach
-	void tearDown()
-	{
-		System.out.println("AfterEach");
-	}
-	
-	@AfterAll
-	static void tearDownAll()
-	{
-		System.out.println("After All");
-	}
-	
-
-}
-
+import static org.junit.Assert.assertEquals;  
+import org.junit.After;  
+import org.junit.AfterClass;  
+import org.junit.Before;  
+import org.junit.BeforeClass;  
+import org.junit.Test;  
+import main.java.Main;  
+  
+public class MainTests {  
+  
+    @BeforeClass  
+    public static void setUpBeforeClass() throws Exception {  
+        System.out.println("before class");  
+    }  
+    @Before  
+    public void setUp() throws Exception {  
+        System.out.println("before");  
+    }  
+  
+    @Test  
+    public void testFindMax(){  
+        System.out.println("test case find max");  
+        assertEquals(4,Main.findMax(new int[]{1,3,4,2}));  
+        assertEquals(43,Main.findMax(new int[]{12,43,4,2}));  
+    }  
+    @Test  
+    public void testCube(){  
+        System.out.println("test case cube");  
+        assertEquals(27,Main.cube(3));  
+    }  
+   
+    
+    @After  
+    public void tearDown() throws Exception {  
+        System.out.println("after");  
+    }  
+  
+    @AfterClass  
+    public static void tearDownAfterClass() throws Exception {  
+        System.out.println("after class");  
+    }  
+  
+} 
